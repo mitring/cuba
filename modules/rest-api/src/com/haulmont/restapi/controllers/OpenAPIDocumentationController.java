a43f6f56d8398e76832fcc8bc729734db988becd
Daniil Tsarev <tsarev@haulmont.com>
2018-03-02 13:26:26 +0400

    PL-9387 Provide project-specific OpenAPI (swagger) documentation for REST API

--- modules/rest-api/src/com/haulmont/restapi/controllers/OpenAPIDocumentationController.java
+++ modules/rest-api/src/com/haulmont/restapi/controllers/OpenAPIDocumentationController.java
@@ -30,6 +30,8 @@ import io.swagger.models.*;
 import io.swagger.models.parameters.BodyParameter;
 import io.swagger.models.parameters.Parameter;
 import io.swagger.models.parameters.QueryParameter;
+import io.swagger.models.parameters.RefParameter;
+import io.swagger.models.properties.StringProperty;
 import org.apache.commons.io.IOUtils;
 import org.dom4j.Element;
 import org.springframework.core.io.Resource;
@@ -48,18 +50,27 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("/v2/openapi_docs")
 public class OpenAPIDocumentationController {
 
+    protected static final String WEB_HOST_NAME = "cuba.webHostName";
+    protected static final String WEB_PORT = "cuba.webPort";
     protected static final String WEB_CONTEXT_NAME = "cuba.webContextName";
 
     protected static final String QUERIES_CONFIG = "cuba.rest.queriesConfig";
     protected static final String SERVICES_CONFIG = "cuba.rest.servicesConfig";
 
     protected static final String QUERY_PATH = "/queries/%s/%s";
+    protected static final String ARRAY_SIGNATURE = "[]";
 
     @Inject
     protected Resources resources;
 
     // cache
     protected Swagger swagger = null;
+
+    protected Map<String, Parameter> parameters = new HashMap<>();
+
+    protected boolean entitiesArePresent = false;
+    protected boolean queriesArePresent = false;
+    protected boolean servicesArePresent = false;
 
     @RequestMapping(value = "/openapi.json", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
     public String getSwaggerJson() {
@@ -97,12 +108,19 @@ public class OpenAPIDocumentationController {
          * todo: use condition "swagger == null"
          */
         swagger = new Swagger()
+                .host(getHost())
                 .basePath(getBasePath())
                 .consumes(APPLICATION_JSON_VALUE)
                 .produces(APPLICATION_JSON_VALUE)
                 .info(generateInfo())
                 .paths(generatePaths())
-                .tags(generateTags(false, true, false));
+                .tags(generateTags(entitiesArePresent, queriesArePresent, servicesArePresent));
+
+        swagger.setParameters(parameters);
+    }
+
+    protected String getHost() {
+        return AppContext.getProperty(WEB_HOST_NAME) + ":" + AppContext.getProperty(WEB_PORT);
     }
 
     protected String getBasePath() {
@@ -111,16 +129,14 @@ public class OpenAPIDocumentationController {
 
     protected Info generateInfo() {
         return new Info()
-                .version("6.9")
-                .title("CUBA Platform REST API")
-                .description("Placeholder description")
+                .version("0.1")
+                .title("Project REST API")
+                .description("Generated Swagger documentation")
                 .license(new License()
                         .name("Apache 2.0")
                         .url("http://www.apache.org/licenses/LICENSE-2.0.html"));
     }
 
-    // todo: remove
-    @SuppressWarnings("SameParameterValue")
     protected List<Tag> generateTags(boolean entitiesArePresent, boolean queriesArePresent, boolean servicesArePresent) {
         List<Tag> tags = new ArrayList<>();
 
@@ -158,6 +174,7 @@ public class OpenAPIDocumentationController {
         if (queriesConfigFiles == null || queriesConfigFiles.isEmpty()) {
             return Collections.emptyMap();
         }
+        queriesArePresent = true;
 
         Map<String, Path> queriesPaths = new HashMap<>();
 
@@ -199,41 +216,75 @@ public class OpenAPIDocumentationController {
 
     protected Path generateQueryPath(String entity, String queryName, List<Pair<String, String>> params) {
         return new Path()
-                .get(generateQueryOperation(RequestMethod.GET, params))
-                .post(generateQueryOperation(RequestMethod.POST, params));
+                .get(generateQueryOperation(entity, queryName, RequestMethod.GET, params))
+                .post(generateQueryOperation(entity, queryName, RequestMethod.POST, params));
     }
 
-    protected Operation generateQueryOperation(RequestMethod method, List<Pair<String, String>> params) {
+    protected Operation generateQueryOperation(String entity, String queryName, RequestMethod method, List<Pair<String, String>> params) {
         Operation operation = new Operation()
                 .tag("Queries")
-                .summary("Execute a query")
+                .summary(queryName)
                 .description("Executes a predefined query. Query parameters must be passed in the request body as JSON map")
                 .response(200, new Response().description("Success"))
-                .response(403, new Response().description("Forbidden. A user doesn't have permissions to read the entity"))
-                .response(404, new Response().description("MetaClass not found"));
+                .response(403, new Response().description("Forbidden. A user doesn't have permissions to read the entity: " + entity))
+                .response(404, new Response().description("MetaClass not found for the entity: " + entity));
 
         params.forEach(param ->
                 operation.addParameter(
-                        generateQueryOperationParam(param, method)));
+                        generateQueryOperationParam(queryName, param, method)));
 
         return operation;
     }
 
-    // todo: consider multiple parameters
-    private Parameter generateQueryOperationParam(Pair<String, String> param, RequestMethod method) {
+    private Parameter generateQueryOperationParam(String queryName, Pair<String, String> param, RequestMethod method) {
+        String paramName = queryName
+                + Character.toUpperCase(param.getFirst().charAt(0))
+                + param.getFirst().substring(1)
+                + "Param"
+                + method.name();
+
+        String ref = "#/parameters/" + paramName;
+
         if (RequestMethod.GET == method) {
-            return new QueryParameter()
-                    .name(param.getFirst())
-                    .type("string");
+            parameters.put(paramName, generateGetQueryOpParam(param));
         } else {
-            return new BodyParameter()
-                    .name(param.getFirst())
-                    .schema(new ModelImpl()
-                            .type(param.getSecond()));
+            parameters.put(paramName, generatePostQueryOpParam(param));
         }
+
+        return new RefParameter(ref);
     }
 
-    // todo: consider arrays
+    protected QueryParameter generateGetQueryOpParam(Pair<String, String> param) {
+        QueryParameter parameter = new QueryParameter()
+                .name(param.getFirst())
+                .required(true);
+
+        if (!param.getSecond().contains(ARRAY_SIGNATURE)) {
+            parameter.type("string");
+        } else {
+            parameter.type("array")
+                    .items(new StringProperty());
+        }
+        return parameter;
+    }
+
+    protected BodyParameter generatePostQueryOpParam(Pair<String, String> param) {
+        BodyParameter parameter = new BodyParameter()
+                .name(param.getFirst());
+        parameter.setRequired(true);
+
+        if (!param.getSecond().contains(ARRAY_SIGNATURE)) {
+            parameter.schema(
+                    new ModelImpl()
+                            .type("string"));
+        } else {
+            parameter.schema(
+                    new ArrayModel()
+                            .items(new StringProperty()));
+        }
+        return parameter;
+    }
+
     @SuppressWarnings("unchecked")
     protected List<Pair<String, String>> parseQueryParams(Element query) {
         Element paramsEl = query.element("params");
@@ -246,8 +297,10 @@ public class OpenAPIDocumentationController {
         List<Pair<String, String>> params = new ArrayList<>();
         for (Element param : (List<Element>) paramsEl.elements("param")) {
             String name = param.attributeValue("name");
+
             String type = param.attributeValue("type");
-            type = type.substring(type.lastIndexOf(".") + 1).toLowerCase();
+            type = type.substring(type.lastIndexOf(".") + 1)
+                    .toLowerCase();
 
             params.add(new Pair<>(name, type));
         }
